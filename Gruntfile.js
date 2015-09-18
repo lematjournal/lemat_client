a7e141fc8029724b732a5ed30a8a45dea6bf729f
idelairre <idelairre@gmail.com>
2015-09-17 20:26:00 -0400

    adds cname file

--- Gruntfile.js
+++ Gruntfile.js
@@ -319,7 +319,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'lematClientApp',
+          module: 'lematClient',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
