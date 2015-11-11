e27d054670c732d34adcb40c44b00a3185aae978
idelairre <idelairre@gmail.com>
2015-11-10 21:22:47 -0500

    changes upsert method in submissions controller so it assembles a 'submission' hash

--- app/scripts/admin/submissions/submissions.admin.controller.js
+++ app/scripts/admin/submissions/submissions.admin.controller.js
@@ -22,7 +22,13 @@
       vm.getSubmission = function () {
          SubFactory.getSubmission($stateParams.submission).then(function () {
             vm.submission = SubFactory.submission;
-            vm.getAttachmentHtml();
+            vm.submission.attachments = vm.submission.attachments
+            //            if (vm.submission.submission_type === 'text') {
+            //               vm.convertDocxToHtml();
+            //            } else if (vm.submission.submission_type === 'image') {
+            //               vm.images = JSON.parse(vm.submission.attachments);
+            //               console.log(vm.images);
+            //            }
          });
       };
 
@@ -31,7 +37,7 @@
             vm.submissions = SubFactory.submissions;
          });
       };
-      
+
       vm.getPendingSubmissions = function () {
          SubFactory.getPendingSubmissions().then(function () {
             vm.pendingSubmissions = SubFactory.pendingSubmissions;
@@ -46,9 +52,23 @@
             }
          });
       };
-      
+
       vm.upsertSubmission = function () {
-         SubFactory.upsertSubmission(vm.submission);
+         var submission = {
+            attachments: vm.submission.attachments,
+            email_address: vm.submission.email_address,
+            email_content: vm.submission.email_content,
+            submission_type: vm.submission.submission_type,
+            tag_names: vm.submission.tags,
+            title: vm.submission.title,
+            uid: vm.submission.uid,
+            user: {
+               username: vm.submission.username
+            }
+         };
+         console.log(submission);
+         console.log(vm.submission);
+         SubFactory.upsertSubmission(submission)
       };
 
       vm.getTags = function () {
@@ -56,7 +76,7 @@
             vm.tags = PostsFactory.tags;
          });
       };
-      
+
       vm.querySubmissions = function (submissions) {
          $scope.disabled = [];
          // need to find a faster way to do this
@@ -70,17 +90,23 @@
             }
          }
       };
-      
-      vm.getAttachmentHtml = function () {
+
+      vm.convertDocxToHtml = function () {
          var deferred = $q.defer();
-         var link = {
+         var params = {
             submission: {
                document: $filter('filterDocs')(vm.submission.attachments)[0]
             }
          };
-         deferred.resolve($http.post(ServerUrl + '/submissions/render-doc', link).then(function (response) {
+         $http.post(ServerUrl + '/submissions/render-doc', params).then(function (response) {
             vm.doc = response.data;
-         }));
+            toastr.success('You can edit your submission in the browser.', 'Preview Generated');
+            deferred.resolve(vm.doc);
+         }, function (errors) {
+            console.log(errors);
+            deferred.reject(errors);
+            toastr.info('This happens when the file format contains irregular data, we\'re working on being able to generate previews in all cases.', 'Could not generate preview');
+         });
          return deferred.promise;
       };
 
@@ -111,7 +137,7 @@
             submissionModal(index);
          }
       };
-      
+
       vm.deleteSubmission = function (id) {
          SubFactory.deleteSubmission(id);
       };
